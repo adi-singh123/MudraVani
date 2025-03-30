97d43d9f83a3bb34e6dde753ba49717ddbc75645
Aditya singh <157194334+adi-singh123@users.noreply.github.com>
2025-03-30 16:52:15 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,14 +1,61 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import {
+  Navbar,
+  Footer,
+  Home,
+  
+  NotFound,
+} from "./components";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const notifyMsg = (type, msg) => {
+  if (type === "success") {
+    const notify = () => toast.success(msg);
+    notify();
+  } else {
+    const notify = () => toast.error(msg);
+    notify();
+  }
+};
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navbar notifyMsg={notifyMsg} />
+      {children}
+      <Footer />
+    </>
+  );
+};
 
 function App() {
+  
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>MudraVani - ISL Recognition</h1>
-      <a href="http://localhost:5000/run-script">
-        <button style={{ padding: "10px 20px", fontSize: "16px" }}>
-          Run Python Script
-        </button>
-      </a>
+    <div className="App">
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <Layout notifyMsg={notifyMsg}>
+              <Home />
+            </Layout>
+          }
+        />
+
+        <Route exact path="*" element={<NotFound />} />
+      </Routes>
+
+      <ToastContainer
+        position="top-left"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        pauseOnHover
+      />
     </div>
   );
 }
